1d149011afe034ccbbb862681a194eb0f63491fd
Alex Reinoso <ralexjessiel@gmail.com>
2022-03-26 23:38:33 -0500

    paso de parametros por url

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola bebe")
+    return HttpResponse("Esta es la pagina de las preguntas")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo el resultado de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
