72b027e298b0dfccf10b8f11c026ac79b2549547
Alex Reinoso <ralexjessiel@gmail.com>
2022-04-27 21:52:24 -0500

    agregado preguntas a index

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Esta es la pagina de las preguntas")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html",{
+        "latest_question_list":latest_question_list
+    })
 
 def detail(request, question_id):
     return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
